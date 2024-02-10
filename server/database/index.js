3796b94f11a71c9bbe2ba982036e5b9e28cf87f7
agniain <agnianurul28@gmail.com>
2024-02-10 09:50:24 +0700

    third

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const {dbHost, dbPort, dbUser, dbPass, dbName} = require('../app/config');
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`);
+mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
 const db = mongoose.connection;
 console.log('database running mystore cloud');
 
