c0217013dc1cf7b486bc5c43038bc016e21c0784
agniain <agnianurul28@gmail.com>
2024-02-12 19:32:28 +0700

    cloud db

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const {dbHost, dbPort, dbUser, dbPass, dbName} = require('../app/config');
+const { dbHost, dbPort, dbUser, dbPass, dbName, dbUrl} = require('../app/config');
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`);
+mongoose.connect(`${dbUrl}://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`);
 const db = mongoose.connection;
 console.log('database running mystore cloud');
 
